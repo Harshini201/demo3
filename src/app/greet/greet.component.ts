6d22c0b0083288936bf4e1e4211c19d9a5ce7ac9
Hasrh <arasuharshini201@gmail.com>
2022-09-26 17:43:03 +0530

    mylogin

--- src/app/greet/greet.component.ts
+++ src/app/greet/greet.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+/*
+  View Encapsulation- hides style of a component from other part.
+  3 strategies:
+ViewEncapsulation.None
+ViewEncapsulation.Emulated-- default
+ViewEncapsulation.ShadowDOM
+*/
+
+
+
+
+import { Component, OnInit,Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-greet',
   templateUrl: './greet.component.html',
+ 
   styleUrls: ['./greet.component.css']
+ /* template: `
+  <h2>None</h2>
+  <div>No encapsulation</div>
+`,
+styles: ['h1 { color: red; }'],
+encapsulation: ViewEncapsulation.None,*/
 })
 export class GreetComponent implements OnInit {
-
+ 
   constructor() { }
-  name: string="Harsha";
-  ngOnInit(): void {
-  }
-  greet(): void{
-    alert("Hello All!!!"+this.name);
-  }
-
+ngOnInit(): void {
   
+}
+username:string="";
+password:string="";
+msg="";
+status:string='error';
+cssStringVar: string= '';
+validate():void{
+  if(this.username=="abc" && this.password=="123"){
+  this.status='';
+    this.msg="Welcome"+this.username;
+  
+  }
+  else{
+  this.msg="Invalid user";
+  this.status='error';
+  
+  }
+  
+  this.username="";
+  this.password="";
+}
+ 
 }
