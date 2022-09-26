e98079381db92ac6d5f7aa0c54d2dcecb7a871e4
Hasrh <arasuharshini201@gmail.com>
2022-09-26 16:43:39 +0530

    Angular

---
+++ src/app/greet/greet.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greet',
+  templateUrl: './greet.component.html',
+  styleUrls: ['./greet.component.css']
+})
+export class GreetComponent implements OnInit {
+
+  constructor() { }
+  name: string="Harsha";
+  ngOnInit(): void {
+  }
+  greet(): void{
+    alert("Hello All!!!"+this.name);
+  }
+
+  
+}
